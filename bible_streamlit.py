26f5521cb4bc08a3b12055ccee828c440c9835d1
fay wu <wru.fay@gmail.com>
2025-12-06 21:03:21 -0500

    Add LLM verse interpretation feature

--- bible_streamlit.py
+++ bible_streamlit.py
@@ -1,3 +1,4 @@
+from openai import OpenAI
 import streamlit as st
 import requests
 
@@ -71,3 +72,50 @@ if search_button:
         st.warning("Please enter a chapter and verse.")
     else:
         st.warning("Please enter both a book name and verse.")
+        
+
+st.markdown("---")
+
+# implement large language model
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+        
+if prompt := st.chat_input("Ask a question here..."):
+    # st.chat_message("user").markdown(f"You: {prompt}")
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # response = f"LLM: {prompt}"
+    # with st.chat_message("assistant"):
+    #     st.markdown(response)
+    # st.session_state.messages.append({"role": "assistant", "content": response})
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    
+
+
+
+        
+        
+        
+        
+    
